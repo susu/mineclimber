efbe5542aacebc332016f1c0de7a943a868cc5a8
Marton Suranyi <marton.suranyi@gmail.com>
2014-01-21 22:14:00 +0100

    ClipRenderer can work with world coordinates

--- include/mine/ClipRenderer.hpp
+++ include/mine/ClipRenderer.hpp
@@ -1,6 +1,7 @@
 #ifndef MINE_CLIPRENDERER_HPP_INC
 #define MINE_CLIPRENDERER_HPP_INC
 
+#include <glm/glm.hpp>
 #include <mine/goodies.hpp>
 #include <mine/Rect.hpp>
 
@@ -9,13 +10,14 @@ namespace mine
   namespace sdl
   {
     class Renderer;
+    class Texture;
   }
   class ClipRenderer : public NonTransferable
   {
     public:
-      ClipRenderer(Rect startClip, sdl::Renderer & renderer);
+      ClipRenderer(FRect startClip, sdl::Renderer & renderer);
 
-      const Rect & getCurrentClip() const
+      const FRect & getCurrentClip() const
       { return m_clip; }
 
       /**
@@ -27,12 +29,15 @@ namespace mine
 
       /**
        * Moves the current clip on Y-axis with yDelta.
-       * yDelta > 0: moves DOWN
-       * yDelat < 0: moves UP
+       * yDelta < 0: moves DOWN
+       * yDelta > 0: moves UP
        */
       void verticalMove(double yDelta);
+
+      void copy(const sdl::Texture & texture, const FRect & to);
     private:
-      Rect m_clip;
+      glm::mat3 m_viewMatrix;
+      FRect m_clip;
       sdl::Renderer & m_renderer;
   };
 }
--- src/ClipRenderer.cpp
+++ src/ClipRenderer.cpp
@@ -1,10 +1,83 @@
 #include <mine/ClipRenderer.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/io.hpp> // for io-streams
+#include <mine/Logger.hpp>
+#include <mine/Enforce.hpp>
 #include <mine/sdl/Renderer.hpp>
+#include <mine/sdl/Texture.hpp>
 
 using namespace mine;
 
-ClipRenderer::ClipRenderer(Rect startClip, sdl::Renderer & renderer)
-  : m_clip(startClip)
-  , m_renderer(renderer)
-{}
+static glm::mat3 createViewMatrixFromRect(FRect rect)
+{
+  glm::mat3 translate = { 1.0, 0.0,                     0.0,
+                          0.0, 1.0,                     0.0,
+                          -rect.m_pos.x, -rect.m_pos.y, 1.0 };
 
+  glm::mat3 scale = { 1.0/rect.m_dimension.x,                  0.0, 0.0,
+                                       0.0, 1.0/rect.m_dimension.y, 0.0,
+                                       0.0,                  0.0, 1.0 };
+  // view * vec3 will result: first translate, then scale
+  glm::mat3 view = scale * translate;
+  return view;
+}
+
+ClipRenderer::ClipRenderer(FRect startClip, sdl::Renderer & renderer)
+  : m_viewMatrix(createViewMatrixFromRect(startClip))
+  , m_clip(startClip)
+  , m_renderer(renderer)
+{
+  LOG_DEBUG(m_viewMatrix);
+}
+
+void ClipRenderer::horizontalMove(double xDelta)
+{
+  m_clip.m_pos.x += xDelta;
+  m_viewMatrix = createViewMatrixFromRect(m_clip);
+}
+
+void ClipRenderer::verticalMove(double yDelta)
+{
+  m_clip.m_pos.y += yDelta;
+  m_viewMatrix = createViewMatrixFromRect(m_clip);
+}
+
+void ClipRenderer::copy(const sdl::Texture & texture, const FRect & to)
+{
+  // Translate
+  glm::vec3 pos = to.m_pos;
+  glm::vec3 dim = to.m_dimension;
+  dim.z = 0; // non-homogen
+
+  // converting coordinates to normalized space
+  glm::vec3 normPos = m_viewMatrix * pos;
+  glm::vec3 normDim = m_viewMatrix * dim;
+
+  Pos winSize = m_renderer.getWindowSize();
+  // converting coordinates to device space
+  glm::mat3 deviceMatrix = { 1.0*winSize.x, 0.0,           0.0,
+                             0.0,           1.0*winSize.y, 0.0,
+                             0.0,                     0.0, 1.0 };
+
+  pos = deviceMatrix * normPos;
+  dim = deviceMatrix * normDim;
+
+  ENFORCE(pos.x > 0, "invalid pos");
+  ENFORCE(pos.y > 0, "invalid pos");
+
+  Rect transformed;
+  transformed.m_pos = pos;
+  transformed.m_dimension = dim;
+
+  // Y-axis direction is inverse
+  transformed.m_pos.y = winSize.y - transformed.m_pos.y;
+
+  // Rect transformed;
+  // transformed.m_pos.x = (to.m_pos.x - m_clip.m_pos.x) * winSize.x / m_clip.m_dimension.x;
+  // transformed.m_pos.y = (to.m_pos.y - m_clip.m_pos.y) * winSize.y / m_clip.m_dimension.y;
+  // transformed.m_dimension.x = to.m_dimension.x * winSize.x / m_clip.m_dimension.x;
+  // transformed.m_dimension.y = to.m_dimension.y * winSize.y / m_clip.m_dimension.y;
+
+  m_renderer.copyTo(texture, transformed);
+}
