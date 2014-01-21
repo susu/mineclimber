2d477ec1a78283bd0c285398c2348da2a1fe00c6
Marton Suranyi <marton.suranyi@gmail.com>
2014-01-21 22:04:56 +0100

    Pos and Rect got some nice feature

--- include/mine/Pos.hpp
+++ include/mine/Pos.hpp
@@ -2,6 +2,7 @@
 #define MINE_PIXELPOS_HPP_INC
 
 #include <ostream>
+#include <glm/glm.hpp>
 
 namespace mine
 {
@@ -9,9 +10,43 @@ namespace mine
   {
     int x;
     int y;
+
+    Pos() : x(0), y(0) {}
+    Pos(int x_, int y_) : x(x_), y(y_) {}
+
+    operator glm::vec3() const
+    { return glm::vec3(x, y, 1); }
+
+    Pos& operator=(const glm::vec3& v)
+    {
+      x = v.x;
+      y = v.y;
+      return *this;
+    }
   };
 
+  inline bool operator==(const Pos& lhs, const Pos& rhs)
+  {
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+  }
+
   inline std::ostream& operator<<(std::ostream & out, const Pos & pos)
+  {
+    return out << "(" << pos.x << ", " << pos.y << ")";
+  }
+
+  struct FPos
+  {
+    double x;
+    double y;
+    FPos() : x(0), y(0) {}
+    FPos(double x_, double y_) : x(x_), y(y_) {}
+
+    operator glm::vec3() const
+    { return glm::vec3(x, y, 1); }
+  };
+
+  inline std::ostream& operator<<(std::ostream & out, const FPos & pos)
   {
     return out << "(" << pos.x << ", " << pos.y << ")";
   }
--- include/mine/Rect.hpp
+++ include/mine/Rect.hpp
@@ -9,7 +9,35 @@ namespace mine
   {
     Pos m_pos;       ///< upper left corner
     Pos m_dimension; ///< size
+
+    Rect() {}
+    Rect(Pos p, Pos d)
+      : m_pos(p)
+      , m_dimension(d)
+    {}
   };
+
+  inline std::ostream& operator<<(std::ostream & out, const Rect & r)
+  {
+    return out << r.m_pos << "; " << r.m_dimension;
+  }
+
+  struct FRect
+  {
+    FPos m_pos;       ///< upper left corner
+    FPos m_dimension; ///< size
+
+    FRect() {}
+    FRect(FPos p, FPos d)
+      : m_pos(p)
+      , m_dimension(d)
+    {}
+  };
+
+  inline std::ostream& operator<<(std::ostream & out, const FRect & r)
+  {
+    return out << r.m_pos << "; " << r.m_dimension;
+  }
 }
 
 #endif
